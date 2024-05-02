e230c67d2cf3ed8de71e88c977242982825ae9d5
rodrigo <digo2112@gmail.com>
2024-05-02 18:39:25 -0300

    A

--- DesafioDeltaFire/Program.cs
+++ DesafioDeltaFire/Program.cs
@@ -19,6 +19,7 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 
 
 
+
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
 
